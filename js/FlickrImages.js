0ce05137be449307b423296d1cd7d6eb7a1f4d5a
Harry Gale <harry.n.gale@gmail.com>
2017-02-25 14:33:13 -0800

    Add more images. Make them bigger to illustrate a point.

--- js/FlickrImages.js
+++ js/FlickrImages.js
@@ -1,38 +1,77 @@
 export const FlickrImages = [
   {
-    uri: 'https://farm3.staticflickr.com/2555/32935423461_6565e880e2_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2555/32935423461_6565e880e2_b.jpg'
   },
   {
-    uri: 'https://farm4.staticflickr.com/3836/32680054080_a28fdcec93_m.jpg'
+    uri: 'https://farm4.staticflickr.com/3836/32680054080_a28fdcec93_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2318/32680010970_33c402d70a_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2318/32680010970_33c402d70a_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2913/32246684973_e81e3b008d_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2913/32246684973_e81e3b008d_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2378/32905987902_926d538a00_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2378/32905987902_926d538a00_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2931/32246674823_a6d7cc29c4_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2931/32246674823_a6d7cc29c4_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2698/32905979432_4bcb0a0b6c_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2698/32905979432_4bcb0a0b6c_b.jpg'
   },
   {
-    uri: 'https://farm4.staticflickr.com/3951/32246661073_1ea604b747_m.jpg'
+    uri: 'https://farm4.staticflickr.com/3951/32246661073_1ea604b747_b.jpg'
   },
   {
-    uri: 'https://farm1.staticflickr.com/764/32246657753_0f817166db_m.jpg'
+    uri: 'https://farm1.staticflickr.com/764/32246657753_0f817166db_b.jpg'
   },
   {
-    uri: 'https://farm1.staticflickr.com/294/32216514194_446e8daafe_m.jpg'
+    uri: 'https://farm1.staticflickr.com/294/32216514194_446e8daafe_b.jpg'
   },
   {
-    uri: 'https://farm3.staticflickr.com/2832/32246649313_edeea711a6_m.jpg'
+    uri: 'https://farm3.staticflickr.com/2832/32246649313_edeea711a6_b.jpg'
   },
-  // {
-  //   uri: ''
-  // },
+  {
+    uri: 'https://farm4.staticflickr.com/3917/32216498564_b1de72a690_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2292/32216496544_7efc1c342d_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2482/32905933232_21472dbd84_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2358/32905929342_7214fe3277_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2697/32905924352_d1bc0ccbe7_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2692/32905919692_211fd2bbd6_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2022/32935386041_a9368ff179_b.jpg'
+  },
+  {
+    uri: 'https://farm1.staticflickr.com/364/32246614173_0e250d8740_b.jpg'
+  },
+  {
+    uri: 'https://farm1.staticflickr.com/284/32935371111_934277a701_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2427/32680013430_67199f8443_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2119/32905889302_11695a4380_b.jpg'
+  },
+  {
+    uri: 'https://farm4.staticflickr.com/3893/32216382994_15e4ba3abc_b.jpg'
+  },
+  {
+    uri: 'https://farm3.staticflickr.com/2324/32935274401_4763d3dd0c_b.jpg'
+  },
+  {
+    uri: 'https://farm1.staticflickr.com/593/32216341504_dbce465964_b.jpg'
+  }
 ]
