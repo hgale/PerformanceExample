2d4fbb3cb83ade434e51553b0a45498287369bea
Harry Gale <harry.n.gale@gmail.com>
2017-02-25 16:20:00 -0800

    Clean up android code.

--- android/app/src/main/java/com/performanceexample/MainApplication.java
+++ android/app/src/main/java/com/performanceexample/MainApplication.java
@@ -1,16 +1,12 @@
 package com.performanceexample;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.performanceexample.ListViewPackage;
 
 import java.util.Arrays;
 import java.util.List;
--- android/app/src/main/java/com/performanceexample/MyViewHolder.java
+++ android/app/src/main/java/com/performanceexample/MyViewHolder.java
@@ -1,17 +1,16 @@
 package com.performanceexample;
 
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
 public class MyViewHolder extends RecyclerView.ViewHolder  {
-    public SimpleDraweeView gifView;
+    public SimpleDraweeView imageView;
 
     public MyViewHolder(View view) {
         super(view);
-        gifView = (SimpleDraweeView) view.findViewById(R.id.my_image_view);
+        imageView = (SimpleDraweeView) view.findViewById(R.id.my_image_view);
 
     }
 }
